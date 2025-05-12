15215c75c9a17c86815b8dbec19b7a4c68e5e7ea
Ngozi Harrison <ngozih@g.ucla.edu>
2025-05-12 12:04:52 -0700

    fixed presentations

--- presentations.js
+++ presentations.js
@@ -1,14 +1,14 @@
 (async () => {
     const response = await fetch('https://api.github.com/repos/ngoziharrison/ngozi-presentations/contents/');
     const data = await response.json();
-    let htmlString = '<ul>';
+    let htmlString = '<ul class="px-0">';
     
     for (let file of data) {
       if(file.path.includes('html')){  
-        htmlString += `<li><a href="${file.path}">${file.name}</a></li>`;
+        htmlString += `<li class = ""><a href="https://ngoziharrison.github.io/ngozi-presentations/${file.path}">${file.name.replace(/\.[^/.]+$/, "")}</a></li>`;
       }
     }
 
     htmlString += '</ul>';
-    document.getElementsByTagName('body')[0].innerHTML = htmlString;
+    document.getElementById('presentations').innerHTML += htmlString;
   })()
